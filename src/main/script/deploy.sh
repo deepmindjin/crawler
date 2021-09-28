8e038bae4bb9bee95112473dad3c9737ab09b7ca
Jang, HongSeok <deepmindjin@gmail.com>
2021-09-29 02:38:04 +0900

    deploy script update to jar file

--- src/main/script/deploy.sh
+++ src/main/script/deploy.sh
@@ -3,4 +3,7 @@
 #$1 aws key
 #$2 file to deploy
 #$3 aws server
-scp -i $1 $2 $3
+AWS_KEY=$1
+FILE=/Users/hongseok/IdeaProjects/crawler/target/crawler-1.0-SNAPSHOT.jar
+AWS_SERVER=$3
+scp -i $AWS_KEY $FILE $AWS_SERVER
